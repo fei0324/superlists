b0603c315c47cc10bf6dba10417aab320509f6c3
fei0324 <fl7834a@student.american.edu>
2018-07-31 19:26:28 -0400

    Added test for the second to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -2,19 +2,18 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import Client
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		# resolve() has the attribute func:
-		# The view function that would be used to serve the URL
-		self.assertEqual(found.func, home_page)
+	def test_uses_home_template(self):
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_home_page_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text':'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
